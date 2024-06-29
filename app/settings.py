4ee09c09ae298b8c34b8b76d720fd262e362d865
Roberto <roberto.rivebarbosa@gmail.com>
2024-06-29 16:30:32 -0300

    config_uswgi and static parans

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1%f+up-x2tr(4fe$64(1a$!%$$c@9y#*tkavz^d07_b6!#l6lb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
